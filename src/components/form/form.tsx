1057e9a20472a9186b5bb372e97fb532b86a7b39
Ivan Ante Traljić <iatraljic@gmail.com>
2022-08-02 19:10:14 +0200

    add form funcionality

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,11 +1,92 @@
+import { ChangeEvent, MouseEvent, useState } from 'react'
+import { ComputationArgs, HashSet } from 'context'
+
+const initialComputationArgs: ComputationArgs = {
+  collectionASize: '',
+  collectionBSize: '',
+  hashSet: HashSet.hashSetA,
+}
+
 function Form() {
+  const [inputs, setInputs] = useState<ComputationArgs>(initialComputationArgs)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name
+    const value = event.target.value
+
+    setInputs((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    console.log(inputs)
+  }
+
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <p>Form</p>
+    <div className='container pt-5'>
+      <form>
+        <div className='row'>
+          <div className='form-group col-sm-4'>
+            <label htmlFor='inputCollectionA'>Size of collection A</label>
+            <input
+              className='form-control'
+              id='inputCollectionA'
+              name='collectionASize'
+              value={inputs.collectionASize}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='form-group col-sm-4'>
+            <label htmlFor='inputCollectionB'>Size of collection B</label>
+            <input
+              className='form-control'
+              id='inputCollectionB'
+              name='collectionBSize'
+              value={inputs.collectionBSize}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='form-group col-sm-4'>
+            <label>To hash-set</label>
+            <div className='form-check'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='hashSet'
+                value={HashSet.hashSetA}
+                checked={inputs.hashSet === HashSet.hashSetA}
+                onChange={handleChange}
+              />
+              <label className='form-check-label' htmlFor='hashSetA'>
+                Collection A
+              </label>
+            </div>
+            <div className='form-check'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='hashSet'
+                value={HashSet.hashSetB}
+                checked={inputs.hashSet === HashSet.hashSetB}
+                onChange={handleChange}
+              />
+              <label className='form-check-label' htmlFor='hashSetB'>
+                Collection B
+              </label>
+            </div>
+          </div>
         </div>
-      </div>
+
+        <div className='form-group row pt-2 pb-5'>
+          <div className='col'>
+            <button className='btn btn-primary w-100' onClick={handleClick}>
+              Start
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,19 +1,33 @@
 import { createContext, useState, FC, PropsWithChildren } from 'react'
 
+// types, interfaces, enums
 export interface IIntersectionContext {
   intersectionSize: number
   intersectionAlgorithmTime: number
 }
 
+export enum HashSet {
+  hashSetA = 'hashSetA',
+  hashSetB = 'hashSetB',
+}
+export interface ComputationArgs {
+  collectionASize: string
+  collectionBSize: string
+  hashSet: HashSet
+}
+
+// initial
 const initialIntersectionContext: IIntersectionContext = {
   intersectionSize: 0,
   intersectionAlgorithmTime: 0,
 }
 
+// createContext
 export const IntersectionContext = createContext<IIntersectionContext>(
   initialIntersectionContext
 )
 
+// IntersectionContextProvider
 const IntersectionContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const [intersectionSize, setIntersectionSize] = useState<number>(
     initialIntersectionContext.intersectionSize
